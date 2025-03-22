13e1aef2810f02f4f744d02e12e55b3973f4c9d4
warrioralok <34855411+warrioralok@users.noreply.github.com>
2025-03-23 00:54:10 +0530

    optimized thunk creator

--- src/Redux/userSlice.js
+++ src/Redux/userSlice.js
@@ -1,23 +1,17 @@
-import {createAsyncThunk, createSlice} from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit";
 
-const fetchUsers = createAsyncThunk(
-    "user/fetchUsers", //unique Action Type
-    async(_,{ rejectWithValue}) =>{
+export const fetchUser = createAsyncThunk(
+    'user/fetchUser',
+    async (_,{rejectWithValue}) =>{
         try {
-            const response = await fetch('https://jsonplaceholder.typicode.com/users');
+            const response = await fetch("https://jsonplaceholder.typicode.com/users");
             if(!response.ok){
-                return rejectWithValue("FailedTo Fetch User List")
+                return rejectWithValue("Failed to fetch user list");
             }
-            return await response.json(); // Return Api Response
-        }
-        catch(error)  {
-            return  rejectWithValue(error.message);
+            return await response.json();
+        } catch (error) {
+                return rejectWithValue(error.message);
         }
     }
 )
 
-const userSlice = createSlice({
-    name:'user',
-    initialState:{users:[],status:'idle', error:null },
-    
-})
